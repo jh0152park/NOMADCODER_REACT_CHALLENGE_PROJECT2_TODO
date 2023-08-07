22053646d63bea8098117134f6ddc04c627d2afc
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-07 23:19:55 +0900

    updated category

--- todo/src/Components/Todo.tsx
+++ todo/src/Components/Todo.tsx
@@ -1,22 +1,35 @@
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 import { ITodo, TodoState } from "../GlobalConfig";
 
-function Todo({ text, category }: ITodo) {
-    const setTodo = useSetRecoilState(TodoState);
+function Todo({ text, category, id }: ITodo) {
+    const [allTodo, setAllTodo] = useRecoilState(TodoState);
 
-    function onButtonClick(category: ITodo["category"]) {}
+    function onButtonClick(category: ITodo["category"], id: ITodo["id"]) {
+        let newOne = allTodo.map((todo) =>
+            todo.id !== id
+                ? todo
+                : {
+                      text: todo.text,
+                      id: todo.id,
+                      category: category,
+                  }
+        );
+        setAllTodo(newOne);
+    }
 
     return (
         <li>
             <span>{text}</span>
             {category !== "Todo" && (
-                <button onClick={() => onButtonClick("Todo")}>Todo</button>
+                <button onClick={() => onButtonClick("Todo", id)}>Todo</button>
             )}
             {category !== "Doing" && (
-                <button onClick={() => onButtonClick("Doing")}>Doing</button>
+                <button onClick={() => onButtonClick("Doing", id)}>
+                    Doing
+                </button>
             )}
             {category !== "Done" && (
-                <button onClick={() => onButtonClick("Done")}>Done</button>
+                <button onClick={() => onButtonClick("Done", id)}>Done</button>
             )}
         </li>
     );
