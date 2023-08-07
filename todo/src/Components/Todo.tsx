c3e7c579847dff0396bbd9bbac5e4fb1d629177a
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-07 22:57:12 +0900

    added button

--- todo/src/Components/Todo.tsx
+++ todo/src/Components/Todo.tsx
@@ -1,12 +1,23 @@
-import { ITodo } from "../GlobalConfig";
+import { useSetRecoilState } from "recoil";
+import { ITodo, TodoState } from "../GlobalConfig";
 
-function Todo({ text }: ITodo) {
+function Todo({ text, category }: ITodo) {
+    const setTodo = useSetRecoilState(TodoState);
+
+    function onButtonClick(category: ITodo["category"]) {}
+
     return (
         <li>
             <span>{text}</span>
-            <button>Todo</button>
-            <button>Doing</button>
-            <button>Done</button>
+            {category !== "Todo" && (
+                <button onClick={() => onButtonClick("Todo")}>Todo</button>
+            )}
+            {category !== "Doing" && (
+                <button onClick={() => onButtonClick("Doing")}>Doing</button>
+            )}
+            {category !== "Done" && (
+                <button onClick={() => onButtonClick("Done")}>Done</button>
+            )}
         </li>
     );
 }
