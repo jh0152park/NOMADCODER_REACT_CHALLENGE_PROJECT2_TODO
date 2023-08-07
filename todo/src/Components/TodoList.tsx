d10cc7834a9103825ff48d635082dc425cd97385
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-07 22:08:29 +0900

    added three different state button at eact item

--- todo/src/Components/TodoList.tsx
+++ todo/src/Components/TodoList.tsx
@@ -1,45 +1,21 @@
-import { useForm } from "react-hook-form";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { TodoState } from "../GlobalConfig";
-
-interface IForm {
-    todo: string;
-}
+import CreateTodo from "./CreateTodo";
+import Todo from "./Todo";
 
 function TodoList() {
-    const { register, setValue, handleSubmit } = useForm<IForm>();
     const [allTodo, setAllTodo] = useRecoilState(TodoState);
-
-    function handleValid(data: IForm) {
-        setAllTodo((oldTodo) => [
-            ...oldTodo,
-            {
-                text: data.todo,
-                category: "Todo",
-                id: Date.now(),
-            },
-        ]);
-        setValue("todo", "");
-    }
 
     return (
         <div>
             <h1>To Dos</h1>
             <hr></hr>
 
-            <form onSubmit={handleSubmit(handleValid)}>
-                <input
-                    {...register("todo", {
-                        required: "Please wirte a Todo",
-                    })}
-                    placeholder="Write a Todo"
-                ></input>
-                <button>Add</button>
-            </form>
+            <CreateTodo />
 
             <ul>
                 {allTodo.map((todo) => (
-                    <li key={todo.id}>{todo.text}</li>
+                    <Todo key={todo.id} {...todo}></Todo>
                 ))}
             </ul>
         </div>
