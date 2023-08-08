a359b46813cfcf6b779ede1271a6ef488646d84d
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-08 20:59:39 +0900

    can add new action item boards

--- todo/src/Components/CreateItem.tsx
+++ todo/src/Components/CreateItem.tsx
@@ -1,17 +1,38 @@
+import { useForm } from "react-hook-form";
 import {
     AddCategoryBoard,
     CreateButton,
     FormContainer,
     Input,
 } from "../Styles/CreateItemStyle";
+import { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { ItemBoardState } from "../GlobalConfig";
+
+interface INewCategory {
+    category: string;
+}
 
 function CreateItem() {
+    const [categoryBoardDisplay, setCategoryBoardDisplay] = useState(0);
+
+    const setAllItemBoard = useSetRecoilState(ItemBoardState);
+
+    const { register, handleSubmit, setValue } = useForm<INewCategory>();
+
     function onCreateNewCategoryButtonClick() {
         console.log("create new category button");
+        setCategoryBoardDisplay((prev) => (prev ? 0 : 1));
     }
 
     function onAddNewActionItemButtonClick() {
         console.log("add new action item button");
+    }
+
+    function onCreateNewCategorySubmit(data: INewCategory) {
+        console.log(data.category);
+        setValue("category", "");
+        setAllItemBoard((prev) => [...prev, data.category as any]);
     }
 
     return (
@@ -23,9 +44,19 @@ function CreateItem() {
                 + Add New Action Item
             </CreateButton>
 
-            <AddCategoryBoard>
-                <form>
-                    <Input placeholder="Please entre new category name"></Input>
+            <AddCategoryBoard
+                style={{
+                    opacity: categoryBoardDisplay,
+                }}
+            >
+                <form onSubmit={handleSubmit(onCreateNewCategorySubmit)}>
+                    <Input
+                        {...register("category", {
+                            required:
+                                "Please enter a new category what you want to add.",
+                        })}
+                        placeholder="Please entre new category name"
+                    ></Input>
                 </form>
             </AddCategoryBoard>
         </FormContainer>
