3ff6ee592a0d43ba1dabfe7fb567edc61bdf3f55
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-09 00:41:47 +0900

    added delete item feature

--- todo/src/Components/ActionItme.tsx
+++ todo/src/Components/ActionItme.tsx
@@ -1,17 +1,27 @@
-import { useRecoilValue } from "recoil";
-import { Item, Items } from "../Styles/ActionItemStyle";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Delete, Item, Items, Move } from "../Styles/ActionItemStyle";
 import { ActionItemState } from "../GlobalConfig";
 
 function ActionItem({ category }: { category: string }) {
-    const allActionItems = useRecoilValue(ActionItemState);
+    const [allActionItems, setAllActionItems] = useRecoilState(ActionItemState);
     const categoryActionItems = allActionItems.filter(
         (item) => item.category === category
     );
 
+    function DeleteActionItem(targetId: number) {
+        console.log("clicked at " + targetId);
+        const NewItems = allActionItems.filter((item) => item.id !== targetId);
+        setAllActionItems(NewItems);
+    }
+
     return (
         <Items>
             {categoryActionItems.map((item) => (
-                <Item key={item.id}>{item.actionItem}</Item>
+                <Item key={item.id}>
+                    {item.actionItem}
+                    <Move></Move>
+                    <Delete onClick={() => DeleteActionItem(item.id)}></Delete>
+                </Item>
             ))}
         </Items>
     );
