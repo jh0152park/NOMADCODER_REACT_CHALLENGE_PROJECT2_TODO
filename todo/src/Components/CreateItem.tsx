5ea4fa930a3e5abb1cb3edf8c5ef8f7f8720263e
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-08-08 16:22:23 +0900

    added create new category borad

--- todo/src/Components/CreateItem.tsx
+++ todo/src/Components/CreateItem.tsx
@@ -1,7 +1,35 @@
-import { FormContainer } from "../Styles/CreateItemStyle";
+import {
+    AddCategoryBoard,
+    CreateButton,
+    FormContainer,
+    Input,
+} from "../Styles/CreateItemStyle";
 
 function CreateItem() {
-    return <FormContainer></FormContainer>;
+    function onCreateNewCategoryButtonClick() {
+        console.log("create new category button");
+    }
+
+    function onAddNewActionItemButtonClick() {
+        console.log("add new action item button");
+    }
+
+    return (
+        <FormContainer>
+            <CreateButton onClick={onCreateNewCategoryButtonClick}>
+                + Create New Category
+            </CreateButton>
+            <CreateButton onClick={onAddNewActionItemButtonClick}>
+                + Add New Action Item
+            </CreateButton>
+
+            <AddCategoryBoard>
+                <form>
+                    <Input placeholder="Please entre new category name"></Input>
+                </form>
+            </AddCategoryBoard>
+        </FormContainer>
+    );
 }
 
 export default CreateItem;
